56e9f35fc79a820573cd86737d689840cdaf4eb5
a-cuc <148326642+a-cuc@users.noreply.github.com>
2024-09-18 07:58:57 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,11 @@
+// Create web server
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var path = require('path');
+var comments = require('./comments.js');
+
+// Create server
+var server = http.createServer(function (req, res) {
+    // Get path
+    var pathname = url.parse(req.url).pathname;
